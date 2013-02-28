764e59048b62887ea747e19e6a20666c3a78f3af
Vincent Chio <vincentchiochio@gmail.com>
2013-02-27 21:46:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiist::Application.config.secret_token = 'b2ece37a5935de4af2e93a0a0872453df3fcb8a7e524939a07c45a561326eaa0027196bb9c5cd50a8f56a54c80267c0560cc796d7439a00a8ec63f4f12051977'
